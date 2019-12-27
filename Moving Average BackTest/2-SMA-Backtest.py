191d2a0d978635bb1ea6681412d3716adc7ae5ba
Raymond Chen <rche0037@student.monash.edu>
2019-12-27 16:36:24 +1100

    Updated SMA BackTest
    Updated to include dates of trades and general clean-up.

--- Moving Average BackTest/2-SMA-Backtest.py
+++ Moving Average BackTest/2-SMA-Backtest.py
@@ -1,14 +1,19 @@
 from binance.client import Client
 import variables as var
-
+from datetime import datetime
+## Adjust your variables in the variables file in the same directory
 client = Client(var.API,var.APIKey)
-dollar = 250
-klines = client.get_historical_klines(var.Pair, Client.KLINE_INTERVAL_1DAY, var.StartDate)#, enddate)
-print("You are trading the",var.BigMA,"vs",var.SmallMA,"moving average on", var.Pair, " since ",var.StartDate)
-old_direction = 'Negative'
+dollar = 1000
+trading = 1000
+klines = client.get_historical_klines(var.Pair, Client.KLINE_INTERVAL_4HOUR, var.StartDate)
+print("You are trading the",var.BigMA,"vs",var.SmallMA,"moving average on"
+      , var.Pair, " since ",var.StartDate, " and ", dollar , " dollars.")
+
+old_direction = 'Null'
 position = 0
+
 for i in range(var.BigMA,len(klines)):
-##Calculate the 10 day klines
+    ##Calculate the small Simple Moving Average klines
     smallMA = 0
     for each in klines[i-var.SmallMA:i]:
         smallMA += float(each[4])
@@ -26,19 +31,36 @@ for i in range(var.BigMA,len(klines)):
         direction = 'Negative'
 
     if old_direction != direction:
-        if old_direction == 'Positive':
-            print("Sell at " + str(each[4]))
-            position += -float(each[4])
-            dollar += float(each[4])
-            #position += -1.01*float(each[4])
+        if old_direction == 'Null':
+            old_direction = direction
         else:
-            print("Buy at " + str(each[4]))
-            position += float(each[4])
-            dollar -= float(each[4])
-            #position += 0.99*float(each[4])
-        old_direction = direction
+            # Determine the date and time of the trade
+            date = datetime.utcfromtimestamp(float(each[6])/1000).strftime('%Y-%m-%d %H:%M:%S')
+            if old_direction == 'Positive' and position != 0:
+                print("Sell at " + str(each[4]))
+                dollar += position * float(each[4]) * (1-var.TakerFee)
+                position = 0
+                if float(each[4]) > buy:
+                    print("Profit = " + str(float(each[4]) - buy) + " Or " + str((round((10000*(float(each[4])-buy)/float(each[4]))))/100) + "% at " + date)
+                else:
+                    print("Loss = " + str(float(each[4]) - buy) + " Or " + str((round((10000*(float(each[4])-buy)/float(each[4]))))/100) + "% at " + date)
+                buy = 0
+            elif old_direction != 'Positive':
+                print("Buy at " + str(each[4]))
+                position += (trading/float(each[4])) * (1-var.MakerFee)
+                dollar -= trading
+                buy = float(each[4])
+            old_direction = direction
+
 if position != 0:
-    position -= float(klines[len(klines)-1][4])
-    dollar += float(klines[len(klines)-1][4])
-    print("Sell at ", str(float(klines[len(klines)-1][4])))
-print("Position is ",str(position))
+    print("Final sell at " + str(float(klines[len(klines)-1][4])))
+    if float(klines[len(klines)-1][4]) > buy:
+        print("Profit = " + str(float(klines[len(klines)-1][4]) - buy) + " Or " + str(
+            (round((10000 * (float(klines[len(klines)-1][4]) - buy) / float(klines[len(klines)-1][4])))) / 100) + "%.")
+    else:
+        print("Loss = " + str(float(klines[len(klines)-1][4]) - buy) + "Or " + str(
+            (round((10000 * (float(klines[len(klines)-1][4]) - buy) / float(klines[len(klines)-1][4])))) / 100) + "%.")
+
+    dollar += position * float(klines[len(klines)-1][4])
+    position = 0
+print("Dollar is ",str(dollar))
---
+++ Moving Average BackTest/variables.py
@@ -0,0 +1,10 @@
+##Note: This API has trading disabled and has been IP locked.
+API = '6VbJxwqwLuCHS4ZD6BP11ZBxIO4t0sg43DDela9UV9aZeKG6HXry9hcgT63RKONP'
+APIKey = 'pct3lKi6ezYb3ej1KmF2dbPEePCtXDjnvpO5Pcov8oDSlIm6K3G78nudsVCSrJcT'
+StartDate = "1 Mar, 2017"
+EndDate = "20 Dec, 2019"
+Pair = "LTCUSDT"
+SmallMA = 21
+BigMA = 55
+MakerFee = 0.00075
+TakerFee = 0.00075
